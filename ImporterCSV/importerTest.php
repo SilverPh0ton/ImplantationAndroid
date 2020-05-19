89ac61f2d5fdb9d8e5f336bc37257bbd2c46ef4d
Guillaume Rivard <dravir08@hotmail.com>
2020-05-19 09:51:35 -0400

    ImporterTest

---
+++ ImporterCSV/importerTest.php
@@ -0,0 +1,19 @@
+<?php
+//Les includes.
+include_once("Entities/BookIdentifier.php");
+include_once("BL/BookImporter.php");
+
+//Déclaration des variables.
+$bookImporter = new BookImporter();
+$booksIdentifiers = array();
+$books = array();
+
+$book1 = new BookIdentifier(20, "9788437624877");
+$book2 = new BookIdentifier(35, "9782253260141");
+
+array_push($booksIdentifiers, $book1);
+array_push($booksIdentifiers, $book2);
+
+$books = $bookImporter->importBooks($booksIdentifiers);
+
+print_r($books);
