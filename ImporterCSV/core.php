237dcb9f803016a4d296c1d2ac9099bb259305dd
Guillaume Rivard <dravir08@hotmail.com>
2020-05-19 12:33:34 -0400

    Tableau dans l'outil importer

--- ImporterCSV/core.php
+++ ImporterCSV/core.php
@@ -4,49 +4,76 @@ include_once 'BL/BookImporter.php';
 include_once 'DBObject/OldDB.php';
 include_once 'DBObject/NewDB.php';
 
-if(isset($_POST["submit"]))
-{
-    try{
+if (isset($_POST["submit"])) {
+    try {
 
 
-    //Objects init
-    $excelImporter = new ExcelImporter();
-    $bookImporter = new BookImporter();
-    $oldDB = new OldDB();
-    $newDB = new NewDB();
+        //Objects init
+        $excelImporter = new ExcelImporter();
+        $bookImporter = new BookImporter();
+        $oldDB = new OldDB();
+        $newDB = new NewDB();
 
-    //Calls
-    $newDB->deleteAll();
-    $extracts = $excelImporter->import($_FILES['file']['name']);
+        //Calls
+        $newDB->deleteAll();
+        $extracts = $excelImporter->import($_FILES['file']['name']);
 
-    $bookIds = $excelImporter->extractBookIdsFromImport($extracts);
-    $booksIdentifiers  = $oldDB->getBooksIdentifers($bookIds);
-    //TODO Get rid of duplicates here And redirect on the single unit
-    //bookImporter has a key(restKey) that must be update if used in the future
-    $bookImporterResponses = $bookImporter->importBooks($booksIdentifiers);
-    //print_r($books);
-    //TODO Manage the url given to download it in our repository and database (with id)
-    $newDB->createBooks($bookImporterResponses->getBooks());
+        $bookIds = $excelImporter->extractBookIdsFromImport($extracts);
+        $booksIdentifiers = $oldDB->getBooksIdentifers($bookIds);
+        //TODO Get rid of duplicates here And redirect on the single unit
+        //bookImporter has a key(restKey) that must be update if used in the future
+        $bookImporterResponses = $bookImporter->importBooks($booksIdentifiers);
+        //Get the old values of book If not found on the ISBN API
+        $unfoundBooks = $oldDB->getBooksFromIds($bookImporterResponses->getUnfoundIds());
+        //TODO Manage the url given to download it in our repository and database (with id)
+        $newDB->createBooks($bookImporterResponses->getBooks());
+        $newDB->createBooks($unfoundBooks);
 
-    //Get the old values of book If not found on the ISBN API
-    $unfoundBooks = $oldDB->getBooksFromIds($bookImporterResponses->getUnfoundIds());
+        //GET USER INFO FROM OLD BD
+        $userIds = $excelImporter->extractUserIdsFromImport($extracts);
 
-    //GET USER INFO FROM OLD BD
-    $userIds = $excelImporter->extractUserIdsFromImport($extracts);
+        //VALIDATE INFO IS CORRECT
+        //SAVE USERS IN NEW BD
 
-    //VALIDATE INFO IS CORRECT
-    //SAVE USERS IN NEW BD
+        //SAVE CONCESSION WITH OLD INFO + ID'S
+        if ($unfoundBooks > 0) {
+            ?>
+            <h3>Livres non trouvés sur l'API et utilisant les vieilles données</h3>
+            <table id="books">
+                <tr>
+                    <td>idBook</td>
+                    <td>title</td>
+                    <td>author</td>
+                    <td>publisher</td>
+                    <td>edition</td>
+                    <td>barcode</td>
+                    <td>urlPhoto</td>
+                </tr>
 
-    //SAVE CONCESSION WITH OLD INFO + ID'S
-    echo 'Livres non trouvés sur l''API et utilisant les vieilles données';
-    foreach($unfoundBooks as $book)
-    {
-        echo $book->getIdBook();
+                <?php
+                for ($i = 0; $i < count($unfoundBooks); $i++) {
+                    ?>
+                    <tr>
+                        <td><?= $unfoundBooks[$i]->getIdBook() ?></td>
+                        <td><?= $unfoundBooks[$i]->getTitle() ?></td>
+                        <td><?= $unfoundBooks[$i]->getAuthor() ?></td>
+                        <td><?= $unfoundBooks[$i]->getPublisher() ?></td>
+                        <td><?= $unfoundBooks[$i]->getEdition() ?></td>
+                        <td><?= $unfoundBooks[$i]->getBarcode() ?></td>
+                        <td><?= $unfoundBooks[$i]->getUrlPhoto() ?></td>
+                    </tr>
+                    <?php
+
+                }
+                ?>
+            </table>
+            <?php
+        }
+
+
+    } catch
+    (Exception $e) {
+        die($e);
     }
-}
-catch(Exception $e)
-{
-    die($e);
-}
 }
 
--- ImporterCSV/index.php
+++ ImporterCSV/index.php
@@ -6,6 +6,35 @@ include_once 'core.php';
   <title>Importer CSV AGECTR</title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+     <style>
+
+         body {
+             padding: 16px;
+         }
+
+         #books {
+             font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+             border-collapse: collapse;
+             width: 100%;
+         }
+
+         #books td, #books th {
+             border: 1px solid #ddd;
+             padding: 8px;
+         }
+
+         #books tr:nth-child(even){background-color: #f2f2f2;}
+
+         #books tr:hover {background-color: #ddd;}
+
+         #books th {
+             padding-top: 12px;
+             padding-bottom: 12px;
+             text-align: left;
+             background-color: #4CAF50;
+             color: white;
+         }
+     </style>
  </head>
  <body>
   <h3 align="center">Import CSV</h3><br />
